61c1e768e203818aeac9a3d25f4bc6dca8443e93
KylevdLangemheen <kylevdlangemheen@live.nl>
2019-02-13 19:02:01 +0100

    Added some classes for the board

--- gamelogic.py
+++ gamelogic.py
@@ -1,2 +1,27 @@
 
 #This is going to contain the game logic for monopoly
+
+class Tile:
+    def __init__(self,tiletype):
+        self.tiletype = tiletype
+        self.players = []
+
+    def moveHere(self,player):
+        player.attile.players.remove(player)
+        player.attile = self
+        self.players.append(player)
+
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.attile = None
+
+class Dummytile:
+    def __init__(self):
+        return
+
+class Board:
+    def __init__(self):
+        self.tiles = []
+        for i in range(40):
+            self.tiles.append(Tile(Dummytile()))
